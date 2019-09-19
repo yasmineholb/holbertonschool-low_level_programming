00c0d52c05f513aa090705864c0ff0ee5d3790d9
yasmineholb <1055@holbertonschool.com>
2019-09-19 19:30:35 +0100

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * main - Start
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c <= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
+}
