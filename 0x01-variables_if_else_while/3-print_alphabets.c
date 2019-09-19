6e1dafda94e65236a51ff52aed1a38e6eedebc53
yasmineholb <1055@holbertonschool.com>
2019-09-19 14:25:20 +0100

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* main - Start
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+return (0);
+}
