bca5398d006b5d0bf54cbc27adc8177201a3a3cb
yasmineholb <1055@holbertonschool.com>
2019-09-19 18:17:19 +0100

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int x = 'a'; x <= 'z'; x++)
+if (c != 'e' && c != 'q')
+putchar(x)
+return (0);
+}
