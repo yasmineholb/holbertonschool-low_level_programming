b4993a9a83892af5420fbfc041c47bbaa123cc1a
yasmineholb <1055@holbertonschool.com>
2019-09-19 21:00:58 +0100

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0 ; x <= 9 ; x++)
+{
+putchar('0' + x);
+if (x != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
