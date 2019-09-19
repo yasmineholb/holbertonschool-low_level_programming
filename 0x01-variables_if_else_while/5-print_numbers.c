e079e40b43f3fab00e9cb73d7d5955cdc8ab1a15
yasmineholb <1055@holbertonschool.com>
2019-09-19 18:46:46 +0100

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 1 ; x < 10 ; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
