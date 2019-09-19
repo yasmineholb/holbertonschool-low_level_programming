3172f43990dc039de31e7272b961ac15a410047b
yasmineholb <1055@holbertonschool.com>
2019-09-19 20:06:48 +0100

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+* if
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
