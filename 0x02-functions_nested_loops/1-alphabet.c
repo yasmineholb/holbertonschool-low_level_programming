6bf90fbd21fa815ab36eb19d060cd5371edc3a9b
yasmineholb <1055@holbertonschool.com>
2019-09-24 11:25:55 +0100

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet - description for this function
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
-char i ;
-for (i='a' ; i<='z' ; i++)
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
 _putchar(i);
 _putchar('\n');
 }
