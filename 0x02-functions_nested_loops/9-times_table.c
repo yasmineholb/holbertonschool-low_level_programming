bf0a32abbd111bc3aace23cbbdd78e246a6b7769
yasmineholb <1055@holbertonschool.com>
2019-09-24 19:21:33 +0100

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * times_table - description for this function
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int n;
+int m;
+int p;
+for (n = 0 ; n <= 9 ; n++)
+{
+_putchar('0');
+for (m = 0 ; m <= 9 ; m++)
+{
+_putchar(',');
+_putchar(' ');
+p = m * n;
+if (p <= 9)
+_putchar(' ');
+else
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+_putchar('\n');
+}
+}
+
