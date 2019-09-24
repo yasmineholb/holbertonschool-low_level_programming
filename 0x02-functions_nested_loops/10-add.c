904828e50fc83266ee6982fda0d75542c045c15b
yasmineholb <1055@holbertonschool.com>
2019-09-24 20:15:39 +0100

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * add - description for this function
+ * @n: parametre int
+ * @m: parametre int
+ * Return: Always 0 (Success)
+ */
+int add(int n, int m)
+{
+int s;
+s = m + n;
+return (s);
+}
