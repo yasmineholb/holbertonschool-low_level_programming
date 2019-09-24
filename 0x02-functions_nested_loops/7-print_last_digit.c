65c13a17c4dac699aef3fcc15759fea522e4a7c4
yasmineholb <1055@holbertonschool.com>
2019-09-24 15:23:56 +0100

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * print_last_digit - description for this function
+ *@n: parameter
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+int s;
+s = n % 10;
+if (s<=0)
+s = s * -1;
+_putchar(s + '0');     
+return (s);
+}
