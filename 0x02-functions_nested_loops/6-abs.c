ccb1b1bbbdd91e1b36151cf338f152c6779c4a1e
yasmineholb <1055@holbertonschool.com>
2019-09-24 14:54:05 +0100

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * _abs - description for this function
+ *@n: parameter
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+int s;
+s = abs(n);
+return (s);
+}
