f2e58b397484aafe6418cec58ecbbd59ded97412
yasmineholb <1055@holbertonschool.com>
2019-09-24 11:30:04 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - description for this function
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-char i ;
-int j=0 ;
-while (j<10)
+char i;
+int j = 0;
+while (j < 10)
 {
-for (i='a' ; i<='z' ; i++)
+for (i = 'a' ; i <= 'z' ; i++)
 _putchar(i);
 _putchar('\n');
 j++;
