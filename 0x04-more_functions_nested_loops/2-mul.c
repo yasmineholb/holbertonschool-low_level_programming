860a3df5d0b4d013f1f0b559ebb6f778af49d00e
yasmineholb <1055@holbertonschool.com>
2019-09-26 11:37:31 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* mul - description for this function
+* @a: parameter
+* @b: parameter
+* Return: Always 0 (Success)
+*/
+int mul(int a, int b)
+{
+int s;
+s = a * b;
+return (s);
+}
