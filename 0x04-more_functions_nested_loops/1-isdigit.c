33d3a749aeba5f76534be9eb9698da55a81a1644
yasmineholb <1055@holbertonschool.com>
2019-09-26 11:23:23 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _isdigit - description for this function
+* @c: parameter
+* Return: Always 0 (Success)
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
