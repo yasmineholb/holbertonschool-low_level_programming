1ab3d465e4a04e922e3dec7f87b0ca945ecb5d8e
yasmineholb <1055@holbertonschool.com>
2019-09-26 12:12:00 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* print_numbers - description for this function
+* Return: Always 0 (Success)
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar('0' + i);
+_putchar('\n');
+}
