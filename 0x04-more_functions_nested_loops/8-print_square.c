47b9688def895a24a99d30f08df6be8c9a6aa4a6
yasmineholb <1055@holbertonschool.com>
2019-09-26 20:01:12 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+* print_square - description for this function
+* @size:parameter
+* Return: Always 0 (Success)
+*/
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+_putchar('\n');
+}
+}
+}
