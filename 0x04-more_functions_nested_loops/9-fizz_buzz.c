6c16f1b196ce39c739a2d7cb93c94007efbe03d9
yasmineholb <1055@holbertonschool.com>
2019-09-26 21:11:39 +0100

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - description for this function
-* Return: Always 0 (Success)
-*/
+ * main - prints "Fizz" for numbers divisible by 3,
+ * prints "Buzz" for numbers divisible by 5,
+ * prints "FizzBuzz" for numbers divisible by 3 and 5
+ * for numbers 1-100
+ * Return: 0;
+ */
 int main(void)
 {
 int i;
 for (i = 1; i <= 99; i++)
 {
-if (i % 3 == 0)
+if (i % 15 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
 printf("Fizz ");
 else if (i % 5 == 0)
 printf("Buzz ");
-else if (i % 15 == 0)
-printf("FizzBuzz ");
 else
-printf("%d ", i);
+printf("%i ", i);
 }
 printf("Buzz\n");
 return (0);
