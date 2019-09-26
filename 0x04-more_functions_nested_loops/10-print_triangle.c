a62288540de49bea9aaebe212bdc2adde7889074
yasmineholb <1055@holbertonschool.com>
2019-09-26 21:25:58 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_triangle - function
+ * @size: triangle height
+ */
+void print_triangle(int size)
+{
+int i, j;
+j = size;
+if (size <= 0)
+_putchar('\n');
+while (size > 0)
+{
+for (i = 0; i < j; i++)
+_putchar((i < size - 1) ? '.' : '#');
+_putchar('\n');
+size--;
+}
+}
