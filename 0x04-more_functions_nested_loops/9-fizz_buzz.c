988bc67d30bb156679da4bfe2c95d9ecb0548925
yasmineholb <1055@holbertonschool.com>
2019-09-26 20:39:56 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - description for this function
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 99; i++)
+{
+if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else if (i % 15 == 0)
+printf("FizzBuzz ");
+else
+printf("%d ", i);
+}
+printf("Buzz\n");
+return (0);
+}
