5425be7d5a1832af067b1b8a683eed37a3820c9f
yasmineholb <1055@holbertonschool.com>
2019-10-01 14:24:21 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*puts_half - function that returns length
+*@str:pointer
+*/
+void puts_half(char *str)
+{
+int i = 0, j;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 != 0)
+{
+for (j = (i - 1) / 2; j < i; j++)
+_putchar(*(str + j));
+}
+else
+{
+for (j = i / 2; j < i; j++)
+_putchar(*(str + j));
+}
+_putchar('\n');
+}
