8d6d17c8c9f178128edf8c068a981d307b41c58e
yasmineholb <1055@holbertonschool.com>
2019-10-01 13:39:08 +0100

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+*puts2 - function that returns length
+*@str:pointer
+*/
+void puts2(char *str)
+{
+int i = 0, j;
+while (str[i] != '\0')
+{
+i++;
+}
+for (j = 0; j <= i; j = j + 2)
+{
+_putchar(*(str + j));
+}
+_putchar('\n');
+}
