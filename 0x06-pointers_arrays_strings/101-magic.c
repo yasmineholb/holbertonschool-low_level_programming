4608e6561d003993f6e20f5cdab9f09441f5c6d2
yasmineholb <1055@holbertonschool.com>
2019-10-03 21:35:48 +0100

    commit

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- *main - function for magic
- *Return: int
- */
 int main(void)
 {
 int n;
@@ -10,7 +6,16 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 *(p + 5) = 98;
+  /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
