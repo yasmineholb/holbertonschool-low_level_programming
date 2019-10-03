a1ea0e42b2bd2b3c71b91907a15812763c2166a7
yasmineholb <1055@holbertonschool.com>
2019-10-03 21:11:13 +0100

    commit

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,26 +1,25 @@
 #include "holberton.h"
 /**
-**rot13 - converts
-* @s: string to convert to rot13
-* Return: returns converted string
-*/
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
 char *rot13(char *s)
 {
-int i;
-i = 0;
-while (s[i] != '\0')
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+for (i = 0; *(s + i); i++)
 {
-while ((s[i] >= 'a' && s[i] <= 'z') ||
-(s[i] >= 'A' && s[i] <= 'Z'))
+for (j = 0; j < 52; j++)
 {
-if ((s[i] >= 'N' && s[i] <= 'Z') ||
-(s[i] >= 'n' && s[i] <= 'z'))
-s[i] -= 13;
-else
-s[i] += 13;
-i++;
+if (a[j] == *(s + i))
+{
+*(s + i) = b[j];
+break;
 }
-i++;
+}
 }
 return (s);
 }
