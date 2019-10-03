b14d3ad912e180fade1c01848dca9823cbf002c0
yasmineholb <1055@holbertonschool.com>
2019-10-03 20:40:02 +0100

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,14 +4,25 @@
  * *_strncpy - function
  *@dest: pointer
  *@src: pointer
+ *@n: longueur
+ *Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i=0;
-while (i < n && *(src + i))
+int i;
+if (n <= 0)
+{
+return (dest);
+}
+i = 0;
+while (i < n && src[i] != '\0')
 {
 dest[i] = src[i];
 i++;
 }
-return (dest);	       
+while (i < n)
+{
+dest[i++] = '\0';
+}
+return (dest);
 }
