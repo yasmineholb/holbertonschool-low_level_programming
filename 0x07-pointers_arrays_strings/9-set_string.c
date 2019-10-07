10129c4ba09218f1795707489459b4c1f977d6d8
yasmineholb <1055@holbertonschool.com>
2019-10-07 16:43:25 +0100

    commit

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - function
+ * @s: parametre
+ * @to: parametre
+ * Return: void.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
