05c65a5f40151b6b9ba4f5532f9d3b76482cf2c3
yasmineholb <1055@holbertonschool.com>
2019-10-21 20:20:02 +0100

    commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ **new_dog - main -fn
+ *struct dog - structure
+ *@name: n
+ *@age: a
+ *@owner: o
+ *Return: dog1
+ *Description: structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog1;
+int i = 0, j = 0, k = 0;
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+dog1 = malloc(sizeof(dog_t));
+if (dog1 == NULL)
+return (NULL);
+dog1->name = malloc(i *sizeof(dog1->name));
+if (dog1 == NULL)
+return (NULL);
+for (k = 0; k < i; k++)
+dog1->name[k] = name[k];
+dog1->age = age;
+dog1->owner = malloc(j *sizeof(dog1->owner));
+if (dog1 == NULL)
+return (NULL);
+for (k = 0; k < j; k++)
+dog1->owner[k] = owner[k];
+return (dog1);
+}
