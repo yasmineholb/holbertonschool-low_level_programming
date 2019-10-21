2fd51d5808d867f8500c3c1c955a84a73fb34e14
yasmineholb <1055@holbertonschool.com>
2019-10-21 21:22:38 +0100

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,16 +14,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog1;
 int i = 0, j = 0, k = 0;
-while (name[i] != '\0')
-i++;
-while (owner[j] != '\0')
-j++;
+while (name[i++] != '\0')
+;
+while (owner[j++] != '\0')
+;
 dog1 = malloc(sizeof(dog_t));
 if (dog1 == NULL)
 {
 return (NULL);
 }
-dog1->name = malloc(i *sizeof(dog1->name));
+dog1->name = malloc(i * sizeof(dog1->name));
 if (dog1 == NULL)
 {
 return (NULL);
@@ -33,7 +33,7 @@ for (k = 0; k < i; k++)
 dog1->name[k] = name[k];
 }
 dog1->age = age;
-dog1->owner = malloc(j *sizeof(dog1->owner));
+dog1->owner = malloc(j * sizeof(dog1->owner));
 if (dog1 == NULL)
 {
 return (NULL);
