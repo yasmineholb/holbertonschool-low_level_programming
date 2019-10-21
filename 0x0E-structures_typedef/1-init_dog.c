678def14c12c832d8b642bb39921ab770f1079cf
yasmineholb <1055@holbertonschool.com>
2019-10-21 14:23:45 +0100

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *init_dog - fn
+ *struct dog - structure
+ *@d: d
+ *@name: n
+ *@age: a
+ *@owner: o
+ *
+ *Description: structure
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog;
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
