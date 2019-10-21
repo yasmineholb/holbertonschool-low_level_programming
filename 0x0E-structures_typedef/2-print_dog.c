e0732bd3aab668b763f09504f05b5dacadb651c2
yasmineholb <1055@holbertonschool.com>
2019-10-21 15:56:06 +0100

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ if (d == NULL)
 return;
 if (d->name == NULL)
 {
-d->name = "(nill)";
+d->name = "(nil)";
 }
 if (d->owner == NULL)
 {
-d->owner = "(nill)";
+d->owner = "(nil)";
 }
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
