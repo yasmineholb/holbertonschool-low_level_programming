3d8d0e18c9ff79ef93ab2baa045296e893dcb65d
yasmineholb <1055@holbertonschool.com>
2019-10-23 12:22:08 +0100

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - prints a name as is
+ * @array: tab
+ * @size: param
+ * @cmp: fn
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}
