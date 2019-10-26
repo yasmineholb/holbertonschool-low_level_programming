3c942fd2d3a07d7bdbcc1ba6d7b08b2b0f04b111
yasmineholb <1055@holbertonschool.com>
2019-10-26 17:28:42 +0100

    commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,16 +11,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list ap;
 unsigned int i;
+char *c;
 va_start(ap, n);
 for (i = 0; i < n; i++)
 {
-printf("%d ", va_arg(ap, char));
+c = va_arg(ap, char *);
+if (c == NULL)
+printf("(nil)");
+else
+printf("%s", c);
 if (separator != NULL && i < n - 1)
-{
 printf("%s", separator);
 }
-}
-va_end(ap);
 putchar('\n');
-
+va_end(ap);
 }
