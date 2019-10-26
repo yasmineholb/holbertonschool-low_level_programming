dafd0a5f307443cb7fa55f698bbefeb8ef743e6c
yasmineholb <1055@holbertonschool.com>
2019-10-26 16:24:46 +0100

    commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list ap;
 unsigned int i;
 va_start(ap, n);
+if (n == 0)
+{
+return;
+}
 for (i = 0; i < n; i++)
 {
 printf("%d ", va_arg(ap, int));
