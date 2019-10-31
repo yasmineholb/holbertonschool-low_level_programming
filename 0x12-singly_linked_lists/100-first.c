ad1fc48dac617e382c08da0c070f3a42f45179cf
yasmineholb <1055@holbertonschool.com>
2019-10-31 21:36:52 +0100

    commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * phrase - fn
+ */
+void phrase(void) __attribute__ ((constructor));
+
+void phrase(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
