38686b73a0d0fb9f32d9d5c641219bd81ec4ead3
yasmineholb <1055@holbertonschool.com>
2019-10-31 21:59:35 +0100

    commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * free_list - fn
- *@head: param
- * Return: Always 0.
+ * free_list - a function
+ * @head: param
  */
 void free_list(list_t *head)
 {
