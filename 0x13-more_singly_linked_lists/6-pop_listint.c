e35ccdc7cd4cbea5dfd145b9accbf408ef686209
yasmineholb <1055@holbertonschool.com>
2019-11-05 09:52:32 +0100

    commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,13 +4,13 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * pop_listint - fn
+ *@head: param
  * Return: Always 0.
  */
 int pop_listint(listint_t **head)
 {
-  int i;
+int i;
 listint_t *k, *j;
 k = *head;
 if (k == NULL)
@@ -19,6 +19,5 @@ j = k;
 i = (*head)->n;
 *head = (*head)->next;
 free(j);
-
 return (i);
 }
