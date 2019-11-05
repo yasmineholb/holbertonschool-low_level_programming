e884625844199ee481222c1a683db5c3078ae781
yasmineholb <1055@holbertonschool.com>
2019-11-05 17:14:56 +0100

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,26 +2,42 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * main - check the code for Holberton School students.
- *
+ *insert_nodeint_at_index - fn
+ *@head: param
+ *@idx: param
+ *@n: param
  * Return: Always 0.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *m, *n;
+listint_t *m, *p;
+unsigned int i = 0;
+if (head == NULL)
+return (NULL);
 m = malloc(sizeof(listint_t));
 if (m == NULL)
 return (NULL);
-  n = *head;
- while (*head)
-   {
-   
-     head->n=index;
-     
-     
-   }
-
- 
+m->n = n;
+p = *head;
+if (idx == 0)
+{
+m->next = (*head)->next;
+(*head)->next = m;
+return (m);
+}
+while (p && (i != idx))
+{
+p = p->next;
+i++;
+if (p == NULL)
+return (NULL);
+if (i == idx - 1)
+{
+m->next = p->next;
+p->next = m;
+return (m);
+}
+}
+return (m);
 }
