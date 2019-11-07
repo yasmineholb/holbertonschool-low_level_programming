418a09b3569c73de13ba27bc304882dd9ec372eb
yasmineholb <1055@holbertonschool.com>
2019-11-07 12:41:31 +0100

    commit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,6 +2,7 @@
 #define file
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
