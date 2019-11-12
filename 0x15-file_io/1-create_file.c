2a6c9bf52228aef78af8dd0ddd04d42cbc03c98c
yasmineholb <1055@holbertonschool.com>
2019-11-12 17:23:39 +0100

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int create_file(const char *filename, char *text_content)
+{
+register int i, j, k;
+k = 0;
+if (filename == NULL)
+return (-1);
+i = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (i == -1)
+return (-1);
+if (text_content)
+{
+while (text_content[k])
+k++;
+j = write(i, text_content, k);
+if (j == -1)
+return (-1);
+}
+close(i);
+return (1);
+}
