ef4ab8ef7365ce0cd49b054eea049233c077142f
yasmineholb <1055@holbertonschool.com>
2019-11-12 17:41:57 +0100

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,3 +7,26 @@
  *
  * Return: Always 0.
  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t i, j, k;
+char *buffer = NULL;
+if (filename == NULL)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+i = open(filename, O_RDONLY);
+if (i == -1)
+return (free(buffer), 0);
+j = read(i, buffer, letters);
+if (j == -1)
+return (free(buffer), 0);
+buffer[letters] = '\0';
+k = write(STDOUT_FILENO, buffer, j);
+if (k == -1)
+return (free(buffer), 0);
+free(buffer);
+close(i);
+return (k);
+}
