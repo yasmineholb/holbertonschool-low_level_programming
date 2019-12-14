51208fcad6f7204ba17308a655ace4895f223eff
yasmineholb <1055@holbertonschool.com>
2019-12-14 16:24:39 +0100

    comm

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,8 +11,8 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new = NULL;
-new = malloc(sizeof(dlistint_t));
+dlistint_t *new;
+new = malloc(sizeof(dlistint_t*));
 if (new == NULL)
 {
 return (NULL);
