9982933c000137c15264fc2de3d35b01f2ab06e2
yasmineholb <1055@holbertonschool.com>
2019-12-15 00:39:38 +0100

    commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,19 +11,14 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new;
-new = malloc(sizeof(dlistint_t *));
-if (new == NULL)
-{
+dlistint_t *ch;
+ch = *head;
+ch = malloc(sizeof(dlistint_t));
+if (ch == NULL)
 return (NULL);
-}
-new->n = n;
-new->prev = NULL;
-new->next = *head;
-if (*head)
-{
-(*head)->prev = NULL;
-}
-*head = new;
-return (new);
+ch->n = n;
+ch->prev = NULL;
+ch->next = *head;
+*head = ch;
+return (ch);
 }
