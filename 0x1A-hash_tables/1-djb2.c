52daa08b90eb482507c9917a2512ec2f2db27a1a
yasmineholb <1055@holbertonschool.com>
2020-01-18 23:49:07 +0100

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ *hash_djb2 - fn djb2
+ *@str: the str
+ *Return: hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) +c;
+}
+return (hash);
+}
