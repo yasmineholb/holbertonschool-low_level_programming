d14182f833902ca5cbedd5bda8a666897b9ec6e1
yasmineholb <1055@holbertonschool.com>
2020-01-19 03:29:15 +0100

    hash

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - fn to get
+ *@key: the key
+ *@ht: hash
+ *Return: hash
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int m;
+hash_node_t *t;
+if (!ht || !key)
+return(NULL);
+m = key_index((unsigned char *)key, ht->size);
+if (ht->array[m])
+{
+t = ht->array[m];
+while(t)
+{
+if strcmp(t->key, key)
+return(t->value);
+t=t->next;
+}
+}
+}
