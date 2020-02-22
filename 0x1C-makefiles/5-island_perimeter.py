62c7bf9e74bb82a65a793f6598f81675354d2cbc
yasmineholb <1055@holbertonschool.com>
2020-02-22 18:11:30 +0100

    fix if statements

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,13 @@ def island_perimeter(grid):
     for i_line, line in enumerate(grid):
         for j_col, col in enumerate(line):
             if col == 1:
-                if grid[i_line][j_col - 1] == 0:
+                if j_col - 1 >= 0 and grid[i_line][j_col - 1] == 0:
                     m += 1
-                if grid[i_line][j_col + 1] == 0:
+                if j_col + 1 < len(grid[i_line]) and grid[i_line][j_col + 1] \
+                == 0:
                     m += 1
-                if grid[i_line - 1][j_col] == 0:
+                if i_line - 1 >= 0 and grid[i_line - 1][j_col] == 0:
                     m += 1
-                if grid[i_line + 1][j_col] == 0:
+                if i_line + 1 < len(grid) and grid[i_line + 1][j_col] == 0:
                     m += 1
     return (m)
