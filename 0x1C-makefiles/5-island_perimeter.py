1db3f327bd0d9bd5db090aaeec71f484f442139a
yasmineholb <1055@holbertonschool.com>
2020-02-22 18:17:52 +0100

    change loops

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,15 @@ island_perimeter module
 def island_perimeter(grid):
     """island_perimeter function """
     m = 0
-    for i_line, line in enumerate(grid):
-        for j_col, col in enumerate(line):
-            if col == 1:
-                if j_col - 1 >= 0 and grid[i_line][j_col - 1] == 0:
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if j - 1 >= 0 and grid[i][j - 1] == 0:
                     m += 1
-                if j_col + 1 < len(grid[i_line]) and grid[i_line][j_col + 1] \
-                == 0:
+                if j + 1 < len(grid[i]) and grid[i][j + 1] == 0:
                     m += 1
-                if i_line - 1 >= 0 and grid[i_line - 1][j_col] == 0:
+                if i - 1 >= 0 and grid[i - 1][j] == 0:
                     m += 1
-                if i_line + 1 < len(grid) and grid[i_line + 1][j_col] == 0:
+                if i + 1 < len(grid) and grid[i + 1][j] == 0:
                     m += 1
     return (m)
