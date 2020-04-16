ef38a231f843dc71ca00744d0205417c068c8125
yasmineholb <1055@holbertonschool.com>
2020-04-16 21:24:46 +0200

    push

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,15 +9,19 @@
 int binary_search(int *array, size_t size, int value)
 {
 int m = 0;
- size_t l = 0;
+size_t l = 0;
 if (!array)
 return (-1);
 m = b_s(array, l, size, value);
-return m;
+return (m);
 }
 /**
-*
-*
+*b_s - fn
+*@array: param
+*@l: param
+*@size: param
+*@value: param
+*Return: int
 */
 int b_s(int *array, size_t l, size_t size, int value)
 {
@@ -28,21 +32,15 @@ mid = (l + size) / 2;
 if (array[mid] == value)
 {
 printf("mid is %li", mid);
-return mid;
+return (mid);
 }
 if (array[mid] > value)
 {
-  for (i = 0; i<size; i++)
-    {
-      printf("%i\n", array[i]);
-    }
-return b_s(array, l, mid-1, value);
+for (i = 0; i < size; i++)
+return (b_s(array, l, mid - 1, value));
 }
-for (i = 0; i<size; i++)
-  {
-    printf("%i\n", array[i]);
-  }
-return b_s(array, mid+1, size-1, value);
+for (i = 0; i < size; i++)
+return (b_s(array, mid + 1, size - 1, value));
 }
 return (-1);
 }
