938bf58003c5e909e661b70a2d9c2b43a508239a
yasmineholb <1055@holbertonschool.com>
2020-04-16 19:11:40 +0200

    header filr

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef fn
+#define fn
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int b_s(int *array, size_t l, size_t size, int value);
+#endif
